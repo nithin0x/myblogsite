907c842ffb10b5adb700ad9b7392bf7aa6be2739
Nithin Kumar <nithin@localhost.localdomain>
2025-09-14 22:12:56 +0530

    minor fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Nithin Kumar',
-  tagline: 'Attacking & Defending - AI |Cloud  | Web | AD ',
+  tagline: 'Attacking & Defending - AI | Cloud  | Web | API | AD ',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
